3cb4e73d0001064cab2b982ad3908504f4722efa
janne.hostikka <janne.hostikka@awacademy.training>
2022-06-08 10:52:20 +0300

    initial

---
+++ shopList_JanneHostikka/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace shopList_JanneHostikka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+
+            Console.WriteLine("Kuinka monta tuotetta tarvitset:" );
+            var tuoteLaskuri = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < tuoteLaskuri; i++)
+
+            {
+
+
+                Console.WriteLine("Syötä tuotteen nimi:");
+                string title = (Console.ReadLine());
+                Console.WriteLine("Tuotteen nimi on:  " + title);
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
