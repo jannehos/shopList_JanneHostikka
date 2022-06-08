a3d9b3cfe2b7092d8de7db7adb7ff335e9cd60ad
janne.hostikka <janne.hostikka@awacademy.training>
2022-06-08 12:36:41 +0300

    tallenna fileen

--- shopList_JanneHostikka/Program.cs
+++ shopList_JanneHostikka/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
 
 namespace shopList_JanneHostikka
 {
@@ -7,24 +10,37 @@ namespace shopList_JanneHostikka
         static void Main(string[] args)
         {
 
+            string file = @"C:\Users\Janne\source\repos\shopList_JanneHostikka\shopList.txt";
 
-            
 
-            Console.WriteLine("Kuinka monta tuotetta tarvitset:" );
-            var tuoteLaskuri = int.Parse(Console.ReadLine());
 
-            for (var i = 0; i < tuoteLaskuri; i++)
+          //  Console.WriteLine("Kuinka monta tuotetta tarvitset:");
+           // var tuoteLaskuri = int.Parse(Console.ReadLine());
+
+           // for (var i = 0; i < tuoteLaskuri; i++)         // Aloitin loopin tekoa jotta saisi tuotteita rajattomasti
+                                                               // mutta jäi kesken 
 
             {
 
 
-                Console.WriteLine("Syötä tuotteen nimi:");
-                string title = (Console.ReadLine());
-                Console.WriteLine("Tuotteen nimi on:  " + title);
+                Console.WriteLine("Syötä tuotteen nimi: ");
 
-                Console.ReadLine();
+
+               // Console.WriteLine("Syötä seuraava tuote: ");
+
+    
+
+
+                using (StreamWriter Lista = File.AppendText(file))
+
+                {
+                    Lista.WriteLine(Console.ReadLine());
+
+
+                }
+
             }
-
         }
+
     }
 }
